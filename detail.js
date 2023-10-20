6e6835c66e5b9dba30a62a775c117c7ab034cdec
Olivier EVRARD <olivier.evrard29@gmail.com>
2023-10-20 09:38:02 +0200

    ajout des commantaires

--- detail.js
+++ detail.js
@@ -13,19 +13,21 @@ getPokemon = async () => {
     // permet de recupere l'id de l'url
     let params = new URLSearchParams(window.location.search);
     let index = params.get("id");
+
     // recupere les infos du pokemon selon l'id
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${index}`);
-    const data = await response.json();
+    const data = await response.json(); // transforme la reponse en JSON
 
     var pokemon_name = data.name; // recupere le nom du pokemon
     var pokemon_sprites_back = data.sprites.back_default // recupere le sprite arriere du pokemon
     var pokemon_sprites_front = data.sprites.front_default // recupere le sprite l'avant du pokemon
-    p_name.textContent = pokemon_name;
-    console.log(pokemon_name)
-    console.log(data)
+    p_name.textContent = pokemon_name; // change le contenu par le nom du pokemon actuelle
+
+    // change la source de l'image par le sprite avant et arriere du pokemon actuelle
     image_back.src = pokemon_sprites_back;
     image_front.src = pokemon_sprites_front;
-    h3_id.textContent = `# ${index}`;
+
+    h3_id.textContent = `# ${index}`; // change le contenu selon l'id du pokemon
 
     // modifications du CSS
     h3_id.style.textAlign = "center"
@@ -44,18 +46,19 @@ getPokemon = async () => {
     div_image.style.backgroundColor = "#ED7D30"
     div_image.style.height = "300px"
     div_image.style.width = "500px"
+    div_image.style.borderRadius = "30px"
+    div_image.style.border = "solid"
+    div_image.style.borderColor = "yellow"
     
     // ajout des balise dans le body
-    
     div_image.appendChild(image_front);
     div_image.appendChild(image_back);
     body.appendChild(h3_id);
     body.appendChild(div_image)
-    body.appendChild(p);
+    body.appendChild(p_name);
     } catch(err) {
         console.error(err);
     }
 }
-
 
 getPokemon();
--- index.js
+++ index.js
@@ -3,31 +3,41 @@
 
 
 const body = document.querySelector("body"); // recupere le body de la page
-const div = document.createElement("div");
+const div = document.createElement("div"); // créer une balise div
 
 
 
-const getPokemons = async () => { // defini la variable async
+const getPokemons = async () => { // defini la variable asynchrone
     try {
         const response = await fetch(` https://pokeapi.co/api/v2/pokemon`); // fetch les pokemon (les 20 premier)
         const data = await response.json(); // transforme la reponse du fetch en reponse JSON
         for (let pokemon of data.results) { // parcours le tableau results
-            const pokemon_url = pokemon.url;
-            console.log(pokemon);
+
+            // recupere l'id dans l'url
+            const pokemon_url = pokemon.url; // stocke l'url du pokemon
             var arr = pokemon_url.split('/');
             var index = arr[6]; // contient l'index
+
             var pokemon_img = document.createElement("img"); // créer une balise img
-            var pokemon_a = document.createElement("a");
+            var pokemon_a = document.createElement("a"); // créer une balise a
             pokemon_img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/0c328e64c6916ec31f9c9ae3a362b0eb9dca66cb/sprites/pokemon/other/dream-world/${index}.svg` // ajoute le lien de l'image selon l'index
+
+            // change la taille des images
             pokemon_img.style.height = "250px"
             pokemon_img.style.width = "250px"
             pokemon_a.style.height = "250px"
             pokemon_a.style.width = "250px"
+
+
             pokemon_img.style.border = "solid"
-            pokemon_a.href = `./detail.html?id=${index}`
+            pokemon_a.href = `./detail.html?id=${index}` // l'adresse du lien change dynamiquement selon l'id du pokemon
             pokemon_a.appendChild(pokemon_img) // ajoute l'image au HTML
+
+            // permet de centrer la div au millieu de la page horizontalement
             div.style.display = "flex"
             div.style.flexWrap = "wrap"
+
+            // ajout de la balise a et de la div à la page html
             div.appendChild(pokemon_a)
             body.appendChild(div)
         }
